cd21140bbf9ecf625f841a068d35d9d9acfce4b0
LetianHub <55523866+LetianHub@users.noreply.github.com>
2024-01-09 20:05:41 +0800

    1

--- opt.py
+++ opt.py
@@ -1,2 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+# construct the function
+start = -5
+stop = 5
+step = 0.1
+
+x = np.arange(start, stop + step, step)
+y = 1 / (1 + np.exp(-x**2)) + (2 * x + 1)**2 / 200 
+
+# gradient descent (1st order)
+
+# newton-type (2nd order)
+
+
+
+plt.plot(x, y)
+plt.show()
 
